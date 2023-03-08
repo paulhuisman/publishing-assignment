f04a567ddf6dccedb87275db22b8e2dac908e251
Paul Huisman <paul.huisman@new10.com>
2023-03-08 13:52:58 +0100

    Deploy React app to GitHub Pages

--- src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DEVICES = {
-    MOBILE: '320px',
-    TABLET_PORTRAIT: '768px',
-    TABLET_LANDSCAPE: '1024px',
-    DESKTOP: '1600px',
-}
--- src/utils/images.ts
+++
@@ -1,5 +0,0 @@
-import { Article } from 'types/Article'
-
-export const getImageCropSrc = (article: Article, cropType: string) => {
-    return article.afbeelding.crops.find((crop) => crop.type === cropType)?.path
-}
